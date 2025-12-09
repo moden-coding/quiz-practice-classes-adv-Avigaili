e8c2d003ac889b378aabaff6b34a83a68258cc0d
Avigail <29akravets@gannacademy.org>
2025-12-08 20:03:02 -0500

    finisheddddd

--- src/StepCounter.java
+++ src/StepCounter.java
@@ -1,3 +1,44 @@
 public class StepCounter {
+    private int minActiveSteps;   
+    private int totalSteps;      
+    private int totalDays;       
+    private int activeDayCount;  
     
+     public StepCounter(int minActiveSteps) {
+        this.minActiveSteps = minActiveSteps;
+        totalSteps = 0;
+        totalDays = 0;
+        activeDayCount = 0;
+    }
+
+
+    public StepCounter() {
+        this.minActiveSteps = 7000;
+        totalSteps = 0;
+        totalDays = 0;
+        activeDayCount = 0;
+    }
+
+
+    public void addDailySteps(int steps) {
+        totalSteps += steps;
+        totalDays++;
+
+        if (steps >= minActiveSteps) {
+            activeDayCount++;
+        }
+    }
+
+    public int activeDays() {
+        return activeDayCount;
+    }
+
+ 
+    public double averageSteps() {
+        if (totalDays == 0) {
+            return 0;
+        }
+        return (double) totalSteps / totalDays;
+    }
+
 }
